137445f3d715ae03c60172687b495c196e5b9fdf
Kenshin38 <makramourguedi@gmail.com>
2020-03-26 11:28:18 +0100

    Fixture job / employee

--- src/DataFixtures/EmployeeFixtures.php
+++ src/DataFixtures/EmployeeFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Employee as EntityEmployee;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EmployeeFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    public function load(ObjectManager $manager){
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++) {
+            $employee = new EntityEmployee();
+            $employee->setFirstname($faker->firstname);
+            $employee->setLastname($faker->lastname);
+            $employee->setEmployementDate($faker->dateTime());
+
+            $manager->persist($employee);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+        Job::class,
+        );
     }
 }
--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Job as EntityJob;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class JobFixtures extends Fixture
+class Job extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $title = new EntityJob();
+        $title->setTitle('Commercial');
+        $manager->persist($title);
+
+        $title = new EntityJob();
+        $title->setTitle('Developpeur');
+        $manager->persist($title);
+
+        $title = new EntityJob();
+        $title->setTitle('Graphiste');
+        $manager->persist($title);
+
+        $title = new EntityJob();
+        $title->setTitle('Designer-Web');
+        $manager->persist($title);
+
+        $title = new EntityJob();
+        $title->setTitle('Support');
+        $manager->persist($title);
 
         $manager->flush();
     }
